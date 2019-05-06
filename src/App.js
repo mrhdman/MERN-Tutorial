23367a01292a638aa8f6b20a2d155d344ad986ec
mrhdman <mattrhavins@gmail.com>
2019-05-06 12:48:46 -0400

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h3>Hello Oxford, Mississippi.</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
